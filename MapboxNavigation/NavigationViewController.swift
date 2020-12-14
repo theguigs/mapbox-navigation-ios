d66a86632ba1d798abb7908dcf6e860c0cffa0fe
Jill Cardamon <jillian.cardamon@mapbox.com>
2020-12-14 09:21:22 -0500

    changed name of authorizationChange boolean for clarity

--- MapboxNavigation/NavigationViewController.swift
+++ MapboxNavigation/NavigationViewController.swift
@@ -41,7 +41,7 @@ open class NavigationViewController: UIViewController, NavigationStatusPresenter
         }
     }
     
-    var authorizationChangeIsFirstCalled = true
+    var didChangeAuthorizationIsFirstCalled = true
     
     /**
      A `Route` object constructed by [MapboxDirections](https://docs.mapbox.com/ios/api/directions/).
@@ -722,13 +722,13 @@ extension NavigationViewController: NavigationServiceDelegate {
         
         if #available(iOS 14.0, *), accuracyAuthorization == .reducedAccuracy {
             addNewStatus(status: authorizationStatus)
-        } else if #available(iOS 14.0, *), previousAuthorizationValue == 1, authorizationChangeIsFirstCalled == false {
+        } else if #available(iOS 14.0, *), previousAuthorizationValue == 1, didChangeAuthorizationIsFirstCalled == false {
             hideStatus(usingStatus: authorizationStatus)
         } else {
             //Fallback on earlier versions
             return
         }
-        authorizationChangeIsFirstCalled = false
+        didChangeAuthorizationIsFirstCalled = false
     }
     
     // MARK: - Building Extrusion Highlighting
