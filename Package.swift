e3664058eda92fea2ce5ff4cd31771c54d4be18b
Minh Nguyễn <mxn@1ec5.org>
2020-12-19 15:57:41 -0800

    MapboxCoreNavigation depends on CMapboxCoreNavigation

--- Package.swift
+++ Package.swift
@@ -12,7 +12,10 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MapboxCoreNavigation",
-            targets: ["MapboxCoreNavigation"]),
+            targets: [
+                "CMapboxCoreNavigation",
+                "MapboxCoreNavigation",
+            ]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -28,6 +31,7 @@ let package = Package(
         .target(
             name: "MapboxCoreNavigation",
             dependencies: [
+                "CMapboxCoreNavigation",
 //                "MapboxAccounts",
                 "MapboxDirections",
 //                "MapboxMobileEvents",
