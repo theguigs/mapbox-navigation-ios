dec8a5d58adcac6fdda9ab20c11bfb3992540ba0
Avi Cieplinski <avi@mapbox.com>
2021-01-15 20:27:15 -0800

    Move Route Duration Annotations to NavigationMapView instead of Example project. Entails also moving a number of class extensions and image resources over as well.

--- MapboxNavigation/UIImage.swift
+++ MapboxNavigation/UIImage.swift
@@ -33,4 +33,29 @@ extension UIImage {
             (text as NSString).draw(in: rect.offsetBy(dx: 0, dy: (constrainedSize.height - font.lineHeight) / 2).integral, withAttributes: textFontAttributes)
         }
     }
+
+    func tint(_ tintColor: UIColor) -> UIImage {
+        let imageSize = size
+        let imageScale = scale
+        let contextBounds = CGRect(origin: .zero, size: imageSize)
+
+        UIGraphicsBeginImageContextWithOptions(imageSize, false, imageScale)
+
+        defer { UIGraphicsEndImageContext() }
+
+        UIColor.black.setFill()
+        UIRectFill(contextBounds)
+        draw(at: .zero)
+
+        guard let imageOverBlack = UIGraphicsGetImageFromCurrentImageContext() else { return self }
+        tintColor.setFill()
+        UIRectFill(contextBounds)
+
+        imageOverBlack.draw(at: .zero, blendMode: .multiply, alpha: 1)
+        draw(at: .zero, blendMode: .destinationIn, alpha: 1)
+
+        guard let finalImage = UIGraphicsGetImageFromCurrentImageContext() else { return self }
+
+        return finalImage
+    }
 }
